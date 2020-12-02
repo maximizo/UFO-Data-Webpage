53cc7037a64770a8bdbcae0d180529030c509478
maximizo <max.izotov96@gmail.com>
2020-12-01 20:38:37 -0500

    level1

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// select tbody
+let tbody = d3.select("tbody");
+
+// make table from provided data
+function buildTable(data){
+    tbody.html("");
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
+           let cell = row.append("td");
+           cell.text(val);
+       });
+    })
+}
+
+// 
+buildTable(tableData);
+
